6f8ed710f896892be888c9a521b4bb4b8955c077
daniel <dsamacag@unal.edu.co>
2025-11-11 22:35:53 -0500

    Nueva vista de horarios disponibles y capacidad de reservar desde alli

--- Proyecto/reservas/urls.py
+++ Proyecto/reservas/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('listar/', views.listar_reservas, name='lista_de_reservas'),
     path('crear/', views.crear_reserva_view, name='crear_reserva'),
+    path('disponibles/', views.horarios_disponibles_view, name='horarios_disponibles'),
 ]
