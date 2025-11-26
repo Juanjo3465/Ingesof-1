6857ff9672e6e1b1010011c900cf8905fc968ed7
juan jose sanchez mora <juanjosemjod@gmail.com>
2025-11-25 23:23:39 -0500

    header app added

--- Proyecto/BuildingAppProject/settings.py
+++ Proyecto/BuildingAppProject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'BuildingAppProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates'), BASE_DIR/'core'/'templates'/'core',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
