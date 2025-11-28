37049fd2c7aca901f0a00799588a396554283452
juan jose sanchez mora <juanjosemjod@gmail.com>
2025-11-28 00:09:40 -0500

    menu added

--- Proyecto/core/services/decorators.py
+++ Proyecto/core/services/decorators.py
@@ -1,12 +1,14 @@
+"""Decoradores utilizados en el proyecto"""
 from functools import wraps
 from django.shortcuts import redirect
-from ..models import Usuario
+from django.views.decorators.cache import never_cache
 
 def login_required(view_func):
+    @never_cache
     @wraps(view_func)
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return redirect("Login")
+            return redirect("Inicial_page")
         return view_func(request, *args, **kwargs)
     return wrapper
 
@@ -18,7 +20,7 @@ def role_required(*allowed_roles):
             user_role = request.user.role
 
             if user_role.get_name() not in allowed_roles:
-                return redirect('Header_user')
+                return redirect('Menu') #Update when the home page be ready
 
             return view_func(request, *args, **kwargs)
         return wrapper
